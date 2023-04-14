b961f10bb9414b984faae52a89989da896e8a6a8
Yavuz Ozturk <mustafayavuzozturk@gmail.com>
2023-04-14 11:13:10 +0000

    small arrangements

--- assignment1/part1.c
+++ assignment1/part1.c
@@ -1,39 +1,35 @@
-
 #include "part1.h"
 #include "sort.h"
 
+// Declarations
 void signal_handler(int sig);
-
 int sig_received = 999;
-struct timeval  sig_rec_time;
+struct timeval sig_rec_time;
 
-void main(int argc, char **argv)
-{
-    
-    int n = atoi(argv[1]) ; // file count
-    srand(time(0));
-    int rand17;
+void main(int argc, char **argv){
 
+    // Declarations
+    int n = atoi(argv[1]); // file count
     pid_t pid[n] ;
     int i, child_status;
 
+    // Random number seed
+    srand(time(0));
+
+    // Set user defined signals
     signal(SIGUSR1, signal_handler);
     signal(SIGUSR2, signal_handler);
 
-    for(i=0; i<n; i++)
-    {
-        rand17 = rand()%7 + 1;
-        if( (pid[i] = fork()) == 0  )
-        {
-            childFunction(i, rand17);
+    for(i=0; i<n; i++){
+        if( (pid[i] = fork()) == 0  ){
+            childFunction(i, rand()%7+1);
             exit(100+i);
         }
     }
 
-    for(i=0; i<n; i++)
-    {
-        if(  (waitpid(pid[i], &child_status, WNOHANG )) == 0 ) // active child
-        {
+    for(i=0; i<n; i++){
+        // determine active child
+        if(  (waitpid(pid[i], &child_status, WNOHANG )) == 0 ){
             if(pid[i]%2 == 0)
                 kill(pid[i], SIGUSR2);
             else
@@ -41,8 +37,7 @@ void main(int argc, char **argv)
         }
     }
 
-    for(i=0; i<n; i++)
-    {  
+    for(i=0; i<n; i++){  
         pid_t wpid = wait(&child_status) ;
         if(WIFEXITED(child_status))
             printf("Child %d terminated with exit status %d \n", wpid, WEXITSTATUS(child_status));
@@ -58,9 +53,9 @@ void signal_handler(int sig) {
     gettimeofday(&sig_rec_time, NULL);
 }
 
-int childFunction(int no, int rand17)
-{
-    // Definitions
+int childFunction(int no, int rand17) {
+
+    // Declarations
     char InputFileName[64], OutputFileName[64];
     int nums[100];
     int m, i;
@@ -68,14 +63,10 @@ int childFunction(int no, int rand17)
     // Get start time
     struct timeval  tv1, tv2;
     gettimeofday(&tv1, NULL);
-    // srand(time(0));
-    // printf("%d %s\n", rand()%10, " rand print");
-    // printf("%ld %s\n\n\n", time(0), " time0");
 
     // Create file names
     sprintf(InputFileName,  "file/input%d.txt",  no);
     sprintf(OutputFileName, "file/output%d.txt", no);
-
 
     // Open files
     FILE *InputFile = fopen(InputFileName, "r");
@@ -101,21 +92,13 @@ int childFunction(int no, int rand17)
         SelectionSort(nums, m);
     else
         InsertionSort(nums, m);
-
-#if 0   
-    for(i=0; i<m; i++)
-        printf("%d ", nums[i]);
-    printf("\n");
-#endif
-    
+   
     // Random sleep between 1-7 seconds
     sleep(rand17);
-    printf("%d%s\n", rand17, "  rand17 print");
 
     // Get end time & calculate execution time
     gettimeofday(&tv2, NULL);
     double time_spent = (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 + (double) (tv2.tv_sec - tv1.tv_sec) ;
-    // printf("%d\n %ld\n", rand17, time(0));
 
 #if 0
     printf ("Total time = %f seconds\n", time_spent);
@@ -146,8 +129,8 @@ int parentFunction(int n) {
     int nums[100];
     float exec_time;
 
-    for(i=0; i<n; i++)
-    {
+    for(i=0; i<n; i++){
+
         sprintf(OutputFileName, "file/output%d.txt", i);
             
         FILE *OutputFile = fopen(OutputFileName, "r");
@@ -166,7 +149,8 @@ int parentFunction(int n) {
         fscanf(OutputFile,"%d\n", &outs[i].m);
 
         fscanf(OutputFile,"%s", outs[i].signal);
-        fscanf(OutputFile,"%s", outs[i].sig_rec); 
+        fscanf(OutputFile,"%s", outs[i].sig_rec);
+
     }
     
     SelectionSortForOuts(outs, n);
@@ -184,13 +168,12 @@ int parentFunction(int n) {
 
     sprintf(FinalOutputFileName, "file/output.txt");
     FILE *FinalOutputFile = fopen(FinalOutputFileName, "w");
-    if (FinalOutputFile == NULL){
+    if (FinalOutputFile == NULL) {
         printf("Cannot open Final output file \n");
         return 1;
     }
 
-    for(i=0; i<n; i++)
-    {
+    for(i=0; i<n; i++){
         fprintf(FinalOutputFile, "%f - ", outs[i].exec_time);
         for(j=0; j<outs[i].m; j++)
             fprintf(FinalOutputFile, "%d ", outs[i].nums[j]);
